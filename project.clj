cf71e5a0f11da7106dd31726c842a664327ab38d
Alistair Dutton <alistair.dutton@ovoenergy.com>
2020-01-12 22:28:18 +0000

    Upgraded deps.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject ovotech/clj-kafka-repl "0.1.0"
-  :dependencies [[aero "1.1.3"]
+  :dependencies [[aero "1.1.4"]
                  [bigsy/clj-nippy-serde "0.1.0"]
                  [cheshire "5.9.0"]
                  [clojure.java-time "0.3.2"]
@@ -7,10 +7,10 @@
                  [mvxcvi/puget "1.2.0"]
                  [org.apache.kafka/kafka-clients "2.3.0"]
                  [org.clojure/clojure "1.10.1"]
-                 [org.clojure/core.async "0.6.532"]
+                 [org.clojure/core.async "0.7.559"]
                  [org.clojure/tools.logging "0.5.0"]
-                 [org.slf4j/slf4j-api "1.7.29"]
-                 [org.slf4j/jul-to-slf4j "1.7.29"]
+                 [org.slf4j/slf4j-api "1.7.30"]
+                 [org.slf4j/jul-to-slf4j "1.7.30"]
                  [ovotech/kafka-avro-confluent "2.1.0-4"]]
 
   :plugins [[mvxcvi/whidbey "2.0.0"]
